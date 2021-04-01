6a191e4afca9b1001652d1a19723c98f90f73794
Ronnrr <13minecraft2@gmail.com>
2021-04-01 12:52:14 +0300

    Defined all necessary functions and variables. Now begining to actually work on them.

--- ViewModel.cs
+++ ViewModel.cs
@@ -1,5 +1,22 @@
 using System;
 
-public class ViewModel {
+public class ViewModel : IObservable {
+	
+	private Model _model; 
 
+	public ViewModel();
+	public void NotifyPropertyChanged(object notifyer, string propertyName);
+	public event NotifyPropertyChanged listeners;
+
+	public double VM_yaw;
+	public double VM_roll;
+	public double VM_pitch;
+	public double VM_direction;
+	public double VM_speed;
+	public int VM_height;
+	
+	public int VM_aileron;
+	public int VM_rudder;
+	public int VM_elevator;
+	public int VM_throttle;
 }
