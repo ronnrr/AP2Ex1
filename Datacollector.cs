77fdedd5541d857edfe819b3ed1ebca085d2e24c
zivnaim <69238109+zivnaim@users.noreply.github.com>
2021-04-02 18:14:37 +0300

    add dataollector - model

---
+++ Datacollector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APEx1
+{
+    class Datacollector
+    {
+        private Model mod; 
+        public Datacollector(Model m)
+        {
+            this.mod = m;
+        }
+
+        // This function get a line from the csv file, 
+        // and update the information in the model. 
+        public void updateData(string s) 
+        {
+            char[] a = { ',' };
+            string[] arr = s.Split(a);
+
+            mod.PAileron = float.Parse(arr[0]); //aileron
+            mod.PElevator = float.Parse(arr[1]); //elevator
+            mod.PRudder = float.Parse(arr[3]); //rudder
+            mod.PThrottle = float.Parse(arr[6]); //throttle
+            mod.PRoll = float.Parse(arr[17]); //roll
+            mod.PPitch = float.Parse(arr[18]); //pitch
+
+
+        }
+    }
+}
