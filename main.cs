c4907484d89ee6932265fa65bf034f46ccf0c3e5
zivnaim <69238109+zivnaim@users.noreply.github.com>
2021-04-04 14:39:04 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APEx1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Model m = new Model();
+            m.observe += func;
+            m.loadFile("C:\\reg_flight.csv");
+            m.play(100, 1);
+
+            //System.Threading.Thread.Sleep(3000);
+            //m.pause();
+        }
+
+        public static void func(object notifyer, string propertyName)
+        {
+            Console.WriteLine(propertyName + " has changed!");
+        }
+    }
+}
