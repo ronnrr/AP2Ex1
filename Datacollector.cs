09b142c9e8ee79fc8d9981766545353174826edb
zivnaim <69238109+zivnaim@users.noreply.github.com>
2021-04-08 20:40:38 +0300

    Update Datacollector.cs

--- Datacollector.cs
+++ Datacollector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,15 +18,23 @@ namespace APEx1
         // and update the information in the model. 
         public void updateData(string s) 
         {
-            char[] a = { ',' };
-            string[] arr = s.Split(a);
+            try
+            {
+                char[] a = { ',' };
+                string[] arr = s.Split(a);
 
-            mod.PAileron = float.Parse(arr[0]); //aileron
-            mod.PElevator = float.Parse(arr[1]); //elevator
-            mod.PRudder = float.Parse(arr[3]); //rudder
-            mod.PThrottle = float.Parse(arr[6]); //throttle
-            mod.PRoll = float.Parse(arr[17]); //roll
-            mod.PPitch = float.Parse(arr[18]); //pitch
+                mod.PAileron = float.Parse(arr[0]); //aileron
+                mod.PElevator = float.Parse(arr[1]); //elevator
+                mod.PRudder = float.Parse(arr[2]); //rudder
+                mod.PThrottle = float.Parse(arr[6]); //throttle
+                mod.PRoll = float.Parse(arr[17]); //roll
+                mod.PPitch = float.Parse(arr[18]); //pitch
+                mod.PYaw = float.Parse(arr[21]); //yaw
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
 
         }
